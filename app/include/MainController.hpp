18157852dfc65b1e91b015bffb29d403aca697bf
Marko-Veljovic <mveljovic2004@gmail.com>
2025-01-21 11:49:28 +0100

    Implement basic GUI

--- app/include/MainController.hpp
+++ app/include/MainController.hpp
@@ -3,6 +3,7 @@
 #include <engine/core/Controller.hpp>
 
 namespace app {
+
     class MainController : public engine::core::Controller {
         void initialize() override;
 
@@ -19,11 +20,13 @@ namespace app {
         void draw() override;
 
         void end_draw() override;
+
     public:
         std::string_view name() const override {
             return "app::MainController";
         }
     };
+
 } // app
 
 #endif //MAINCONTROLLER_HPP
