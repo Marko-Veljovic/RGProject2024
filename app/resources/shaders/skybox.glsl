e14d84faf11d8c455f006255e24bb658b832e660
Marko-Veljovic <mveljovic2004@gmail.com>
2025-02-22 11:01:25 +0100

    tried to remove moon volumetric light using alpha RGBA component

--- app/resources/shaders/skybox.glsl
+++ app/resources/shaders/skybox.glsl
@@ -28,7 +28,7 @@ void main()
     FragColor = texture(skybox, TexCoords);
     float brightness = dot(FragColor.rgb, vec3(0.2126, 0.7152, 0.0722));
     if (brightness > 0.558) {
-        BrightColor = vec4(FragColor.rgb, 1.0);
+        BrightColor = vec4(FragColor.rgb, 0.9);
     }
     else {
         BrightColor = vec4(0.0, 0.0, 0.0, 1.0);
